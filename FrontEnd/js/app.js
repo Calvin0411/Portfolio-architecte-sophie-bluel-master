ac16ef70535083f043dfc078b07ca485bc90c792
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-08-07 12:47:02 +0200

    Ajout des filtres
    J'ai ajouté une div contenant ma partie sur mon filtre

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -9,7 +9,7 @@ async function getWorks (){
         console.log(json);
        
         for (let i = 0; i < json.length; i++) {
-            setFigure(json[i]);
+            displayWorks(json[i]);
         }
     } catch (error) {
         console.error(error.message);
@@ -19,9 +19,44 @@ async function getWorks (){
 getWorks();
 
 // Fonction pour créer soit ajouter l'élément figure dans la galerie ansi que ça description
-function setFigure(data) {
+function displayWorks(data) {
     const figure = document.createElement("figure");
     figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
                       <figcaption>${data.title}</figcaption>`;          
     document.querySelector(".gallery").append(figure);
   }
+
+//récupération de mon tableau de catégories
+
+  async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+        const response = await fetch (url);
+        const json = await response.json ();
+        console.log (json);
+        
+        for (let i = 0; i< json.length; i++){
+            displayCategories(json[i]);
+        }
+
+    } catch (error) {
+        console.error(error.message);
+    }
+  }
+
+getCategories();
+
+function displayCategories (data){
+    const div = document.createElement("div");
+    
+    div.innerHTML = `${data.name}`;
+    document.querySelector (".div-container").append(div)
+    
+}
+
+
+
+function filterWorkByCategory(categoryId){
+
+}
+
